b844738633540dee4953364b3e918cab2f8009f0
sadyrbekovadina-droid <sadyrbekovadina@gmail.com>
2026-01-12 20:31:42 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Idea.bkz", layout="centered")
+
+st.title("Idea.bkz")
+st.subheader("Интеллектуальная платформа обратной связи")
+st.write("Биология, 7 класс")
+
+st.markdown("---")
+st.success("Python установлен")
+st.success("Приложение запущено")
+st.info("Это тестовый экран. Дальше здесь будет ваш проект для Daryn AI.")
